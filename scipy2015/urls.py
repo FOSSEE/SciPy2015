34b882cfea23621a0a5d6b83f4ce50862a915a2a
hardythe1 <hardy_the1@yahoo.com>
2015-07-07 17:24:02 +0530

    change urls

--- scipy2015/urls.py
+++ scipy2015/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^2015/', include('website.urls', namespace='website')),
+    url(r'^', include('website.urls', namespace='website')),
     url(r'^', include('social.apps.django_app.urls', namespace='social')),
     url(r'^', include('django.contrib.auth.urls', namespace='auth')),
 )
